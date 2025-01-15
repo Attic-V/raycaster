a9abb3094b64ff597327580e082cd41ae74f591c
Attic V <106038832+Attic-V@users.noreply.github.com>
2025-01-14 22:39:50 -0600

    create window

---
+++ include/window.h
@@ -0,0 +1,7 @@
+#ifndef window_h
+#define window_h
+
+#define WINDOW_WIDTH 1000
+#define WINDOW_HEIGHT (WINDOW_WIDTH / 1.618)
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,2 +1,31 @@
+#include <stdbool.h>
+
+#include <SDL2/SDL.h>
+
+#include "window.h"
+
 int main (void)
-{ }
+{
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_Window *window = SDL_CreateWindow(
+		"raycaster",
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		WINDOW_WIDTH, WINDOW_HEIGHT,
+		SDL_WINDOW_BORDERLESS
+	);
+
+	bool quit = false;
+	while (!quit) {
+		SDL_Event event;
+		SDL_PollEvent(&event);
+		quit =
+			(event.type == SDL_QUIT) ||
+			(event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE)
+		;
+	}
+
+	SDL_DestroyWindow(window);
+
+	SDL_Quit();
+}
