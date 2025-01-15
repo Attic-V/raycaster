4762de5a3fd2f572a56dcdc2cd23ab6bb9ab52f3
Attic V <106038832+Attic-V@users.noreply.github.com>
2025-01-14 23:02:51 -0600

    render map

--- src/main.c
+++ src/main.c
@@ -2,7 +2,10 @@
 
 #include <SDL2/SDL.h>
 
+#include "map.h"
 #include "window.h"
+
+void render (SDL_Renderer *renderer);
 
 int main (void)
 {
@@ -15,6 +18,8 @@ int main (void)
 		SDL_WINDOW_BORDERLESS
 	);
 
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+
 	bool quit = false;
 	while (!quit) {
 		SDL_Event event;
@@ -23,9 +28,33 @@ int main (void)
 			(event.type == SDL_QUIT) ||
 			(event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE)
 		;
+
+		render(renderer);
 	}
 
 	SDL_DestroyWindow(window);
 
 	SDL_Quit();
+}
+
+void render (SDL_Renderer *renderer)
+{
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+	SDL_RenderClear(renderer);
+
+	static const int mapscale = WINDOW_HEIGHT / MAP_HEIGHT;
+	SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0);
+	for (int i = 0; i < MAP_HEIGHT; i++) {
+		for (int j = 0; j < MAP_WIDTH; j++) {
+			if (!map[i][j]) continue;
+			SDL_RenderFillRect(renderer, &(SDL_Rect){
+				.x = j * mapscale,
+				.y = i * mapscale,
+				.w = mapscale,
+				.h = mapscale,
+			});
+		}
+	}
+
+	SDL_RenderPresent(renderer);
 }
